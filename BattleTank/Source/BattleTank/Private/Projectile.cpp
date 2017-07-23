73f5706065500d366b5626007862f335d6a10882
DESKTOP-B8PD6UE\Michal <gluski.m@gmail.com>
2017-07-23 14:11:25 +0200

    Set Throttle

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -29,7 +29,7 @@ void AProjectile::Tick(float DeltaTime)
 void AProjectile::LaunchProjectile(float Speed)
 {
 
-	UE_LOG(LogTemp, Warning, TEXT("Projectile %f"), Speed)
+	
 	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
 	ProjectileMovement->Activate();
 }
